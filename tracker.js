f9dd7e72240ad9fec3b8ca985ce7ee9c6e1ca55e
xturka <xturkabet@gmail.com>
2025-02-26 15:56:52 +0300

    js5

--- tracker.js
+++ tracker.js
@@ -1,34 +1,16 @@
 async function getRedirectUrl() {
     try {
-        const proxyUrl = "https://api.allorigins.win/get?url="; // CORS Proxy
-        const trackerUrl = encodeURIComponent("https://cutt.ly/le6JAfIP");
+        const proxyUrl = "https://api.allorigins.win/raw?url="; // Alternatif Proxy
+        const trackerUrl = "https://cutt.ly/le6JAfIP";
 
-        const response = await fetch(proxyUrl + trackerUrl);
-        const data = await response.json();
+        const response = await fetch(proxyUrl + trackerUrl, {
+            method: "HEAD",
+            redirect: "follow",
+        });
 
-        console.log("Cutt.ly yönlendirme sayfası HTML içeriği:", data.contents);
+        console.log("Gerçek yönlendirme URL'si:", response.url);
 
-        // Sayfanın HTML içeriğini DOM olarak işle
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(data.contents, "text/html");
-
-        // Eğer sayfada meta refresh ile yönlendirme varsa, onu al
-        const metaRefresh = doc.querySelector("meta[http-equiv='refresh']");
-        if (metaRefresh) {
-            const redirectUrl = metaRefresh.getAttribute("content").split("=")[1]; // URL'yi al
-            document.getElementById("redirected-url").innerHTML = `<a href="${redirectUrl}" target="_blank">${redirectUrl}</a>`;
-            return;
-        }
-
-        // Eğer meta refresh yoksa, og:url içeriğine bak
-        const ogUrl = doc.querySelector("meta[property='og:url']");
-        if (ogUrl) {
-            const redirectUrl = ogUrl.getAttribute("content");
-            document.getElementById("redirected-url").innerHTML = `<a href="${redirectUrl}" target="_blank">${redirectUrl}</a>`;
-            return;
-        }
-
-        document.getElementById("redirected-url").innerText = "Yönlendirme adresi bulunamadı!";
+        document.getElementById("redirected-url").innerHTML = `<a href="${response.url}" target="_blank">${response.url}</a>`;
 
     } catch (error) {
         console.error("Hata:", error);
