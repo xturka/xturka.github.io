1eff9b1d1ed256f534a7d6e7a4b49ee6480626bf
xturka <xturkabet@gmail.com>
2025-02-26 15:21:36 +0300

    proxy

--- tracker.js
+++ tracker.js
@@ -4,19 +4,30 @@ async function getLatestSite() {
         const trackerUrl = encodeURIComponent("https://cutt.ly/le6JAfIP");
 
         const response = await fetch(proxyUrl + trackerUrl);
-        const data = await response.json();
+        const data = await response.json();  // JSON formatında cevabı al
 
-        // Response içindeki URL'yi bul
+        // Sayfanın HTML içeriğini al
         const parser = new DOMParser();
         const doc = parser.parseFromString(data.contents, "text/html");
-        const metaRefresh = doc.querySelector("meta[http-equiv='refresh']");
 
+        // Meta Refresh etiketini bul
+        const metaRefresh = doc.querySelector("meta[http-equiv='refresh']");
         if (metaRefresh) {
-            const redirectUrl = metaRefresh.getAttribute("content").split("=")[1];
+            const redirectUrl = metaRefresh.getAttribute("content").split("=")[1];  // URL'yi al
             document.getElementById("latest-site").innerHTML = `<a href="${redirectUrl}" target="_blank">${redirectUrl}</a>`;
-        } else {
-            document.getElementById("latest-site").innerText = "Bağlantı alınamadı!";
+            return;
         }
+
+        // Eğer meta refresh yoksa, `og:url` içeriğine bak
+        const ogUrl = doc.querySelector("meta[property='og:url']");
+        if (ogUrl) {
+            const redirectUrl = ogUrl.getAttribute("content");
+            document.getElementById("latest-site").innerHTML = `<a href="${redirectUrl}" target="_blank">${redirectUrl}</a>`;
+            return;
+        }
+
+        // Eğer hiçbir yönlendirme bulunamazsa hata ver
+        document.getElementById("latest-site").innerText = "Bağlantı bulunamadı!";
 
     } catch (error) {
         console.error("Hata:", error);
